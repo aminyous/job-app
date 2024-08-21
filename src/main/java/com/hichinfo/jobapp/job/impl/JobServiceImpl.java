7f07222f24e775397b0cce124dd78e9b52011ede
Amine Hichri <amyn.hichri@gmail.com>
2024-08-21 15:13:03 +0200

    first commit

---
+++ src/main/java/com/hichinfo/jobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,69 @@
+package com.hichinfo.jobapp.job.impl;
+
+import com.hichinfo.jobapp.job.Job;
+import com.hichinfo.jobapp.job.JobRepository;
+import com.hichinfo.jobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+
+
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(Job job) {
+
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job findById(Long id) {
+       return jobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+
+        try{
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if(jobOptional.isPresent()){
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setLocation(updatedJob.getLocation());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setMinSalary(updatedJob.getMinSalary());
+            jobRepository.save(job);
+            return true;
+            }
+        return false;
+        }
+
+
+
+}
